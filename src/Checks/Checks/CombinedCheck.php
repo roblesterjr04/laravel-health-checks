8eefe804c04c2822c86e15b7c6a64280e50b1c70
Robert Lester <robertlesterjr@mac.com>
2023-06-23 12:21:42 -0400

    Update CombinedCheck.php

--- src/Checks/Checks/CombinedCheck.php
+++ src/Checks/Checks/CombinedCheck.php
@@ -7,15 +7,23 @@ use Lester\Health\Checks\Result;
 
 class CombinedCheck extends Check
 {
-    
+
     private $checkIcons = [
         "ok" => "✅",
         "warning" => "⚠️",
         "failed" => "⛔️",
     ];
-    
+
+    protected $cutoff = 20;
+
     protected $checks = [];
-    
+
+    public function truncateSummary($n)
+    {
+        $this->cutoff = $n;
+        return $this;
+    }
+
     public function run(): Result
     {
         $notOk = 0;
@@ -23,27 +31,29 @@ class CombinedCheck extends Check
 
         $result = Result::make();
         $summary = [];
-            
+
         foreach ($this->checks as $check) {
             $checkResult = $check->run();
             $status = (string)$checkResult->status;
             if ($status !== 'ok') $notOk++;
-            $summary[] = $this->checkIcons[(string)$checkResult->status] . " {$check->getName()}" . substr($checkResult->getNotificationMessage(), 0, 20);
+            $summary[] = $this->checkIcons[(string)$checkResult->status] . " {$check->getName()}: " .
+                substr($checkResult->getShortSummary(), 0, $this->cutoff);
         }
-        
-        $result->ok(implode("<br />", $summary));
+
+        $message = implode("<br />", $summary);
+        $result->shortSummary($message);
 
         if ($notOk == $total) return $result->failed();
         if ($notOk > 0) return $result->warning();
 
-        return $result;
+        return $result->ok();
 
     }
-    
+
     public function using(array $checks): self
     {
         $this->checks += $checks;
-        
+
         return $this;
     }
 
