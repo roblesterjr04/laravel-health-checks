937288451db784a1dd737db398a1fa050ce37516
Robert Lester <rlester@tcsvc.com>
2024-01-10 14:27:59 -0500

    added gibhub actions test

---
+++ src/Checks/Checks/GitHubActionsCheck.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Lester\Health\Checks\Checks;
+
+use Lester\Health\Checks\Check;
+use Lester\Health\Checks\Result;
+use GuzzleHttp\Client;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
+
+class GitHubActionsCheck extends Check
+{
+	
+	private $action;
+	private $repo;
+	private $owner;
+	
+	public function run(): Result
+	{
+		$result = Result::make();
+		$response = Http::withToken(config('health-exp.mailgun.secret') ?: '')
+			->get("https://api.github.com/repos/{$this->owner}/{$this->repo}/actions/runs");
+			
+		$array = $response->json('workflow_runs');
+		
+		foreach ($array as $run) {
+			if ($run['name'] === $this->action) {
+				if ($run['conclusion'] == 'failure') return $result->failed();
+			}
+		}
+		
+		return $result;
+	}
+	
+	public function action($name)
+	{
+		$this->action = $name;
+		return $this;
+	}
+	
+	public function owner($owner)
+	{
+		$this->owner = $owner;
+		return $this;
+	}
+	
+	public function repo($repo)
+	{
+		$this->repo = $repo;
+		return $this;
+	}
+	
+}
--- tests/GitHubTest.php
+++ tests/GitHubTest.php
@@ -3,7 +3,9 @@
 namespace Tests;
 
 use Lester\Health\Checks\Checks\GitHubCheck;
+use Lester\Health\Checks\Checks\GitHubActionsCheck;
 use Lester\Health\Checks\Checks\TerminalCheck;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class GitHubTest extends TestCase
@@ -26,6 +28,33 @@ class GitHubTest extends TestCase
 		$result = $check->command('cd ~/GitHub/wotc && php artisan test')->run();
 		
 		$this->assertInstanceOf('Lester\Health\Checks\Result', $result);
+	}
+	
+	public function testPingRedirectCheck()
+	{
+		
+		Http::fake([
+			'https://api.github.com/repos/*' => Http::response([
+				'workflow_runs' => [
+					[
+						'name' => 'skip me',
+					],
+					[
+						'name' => 'Test Flow',
+						'conclusion' => 'failure'
+					]
+				]
+			]),
+		]);
+			
+		$check = GitHubActionsCheck::new()
+			->owner('tester')
+			->repo('test')
+			->action('Test Flow');
+		
+		$result = $check->run();
+		$this->assertEquals($result->status, 'failed');
+		
 	}
 
 }
